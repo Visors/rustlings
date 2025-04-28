17b35cd6123d64cf0d11db009aec914466be844a
Visors <416815311@qq.com>
2025-04-28 17:14:45 +0800

    play exercises/04_primitive_types

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -10,7 +10,9 @@ mod tests {
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
         // let nice_slice = ???
+        let nice_slice = &a[1.. 4];
 
-        assert_eq!([2, 3, 4], nice_slice);
+        assert_eq!([2, 3, 4], nice_slice); // 因为 Rust 自动为 [T; N] 和 &[T] 实现了相互比较，所以 assert_eq! 可以直接拿 &[i32] 用，不用手动解引用。
     }
 }
+
